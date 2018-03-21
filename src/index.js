20e57f8fb48278ebaf9ff6faddbd11a8d33a8f5f
gossoudarev <igossoudarev@herzen.spb.ru>
2018-03-21 19:48:52 +0300

    middleware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@
 const express = require('express');
 
 const PORT = 4321;
-const app = express(); // God-like singleton object Application
-
+const app = express(); // здесь мы просто показываем как вызываются функции middleware
+// они вызываются экспрессом так же как DOM вызывает обработчики щелчков и прочих событий
 app
-  .get('/', r => r.res.send('Вам привет от Express!'))
+  .use('/h', (...n) => {
+    console.log('h!');
+    console.log(n.length);
+    n[1].send('h!');
+  })
+  .use((...n) => {
+    console.log(`обработка запроса для ${n[0].url} ....`);
+    n[2]();
+  })
+  .use((r, res, next) => {
+    console.log('завершаем запрос');
+    res.send('Спасибо за игру!');
+  })
+
   .get('/hello', r => r.res.send('Привет, неизвестный!'))
   .get('/hello/:name', r => r.res.send(`Привет, ${r.params.name}!`))
   .use(r => r.res.set({
@@ -13,4 +26,5 @@ app
     'Content-Type': 'text/html; charset=utf-8',
   }).status(404).end('Тут пока ничего нет, сорри!'))
   .use((e, r, res, n) => r.res.status(500).end(`Ошибочка: ${e}`))
+  /* видимо экспресс смотрит, какая из функций объявлена с 4-мя аргументами см. fakeapp.js */
   .listen(process.env.PORT || PORT, () => console.log('Я запущен'));
